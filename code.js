871020baceb190b10540921f304291db1dc13e17
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-12-05 17:51:41 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,35 +3,36 @@ import java.util.PriorityQueue;
 function dijkstra(graph, sourceNode) {
     var DijMatrix = [];
     boolean visited = [];
-    var check;
+    var check, dist;
     var inf = 999;
     PriorityQueue<integer> priority = new PriorityQueue<>();
 
     for (var i = 0; i < graph.length; i++) {
         if (i == 0){
             DijMatrix[i] = 0;
-            visited.pop(true);
+            visited.push(true);
         }
         else {
             DijMatrix[i] = inf;
-            visited.pop(false);
+            visited.push(false);
         }
     }
-    priority.add[0, 0];
+    priority.add(0, 0);
 
     while (!priority.isEmpty()) {
         check = priority.poll();
         for (var i = 0; i < graph.length; i++) {
-            if (graph[i] > 0) {
-                dist = DijMatrix[check] + graph[i];
+            if (graph[check][i] > 0) {
+                dist = DijMatrix[check] + graph[check][i];
                 if (dist < DijMatrix[i]) {
                     DijMatrix[i] = dist;
                     if (!visited[i]) {
-                        priority.add[i, dist];
+                        priority.add(i, DijMatrix[i]);
                         visited[i] = true;
                     }
                 }
             }
         }
+    }
     return DijMatrix;
 }
